25318b4b2f34b65ac0732f52246f2aca9daf2aac
euanthieu <euanthieu@gmail.com>
2024-06-20 22:39:41 +0800

    Made Routes for Home, About Me, Hobbies and Skills

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutMeController;
+use App\Http\Controllers\SkillsController;
+use App\Http\Controllers\HobbiesController;
+
+//Home page
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
+// Route for "About Me" page
+Route::get('/about_me', [AboutMeController::class, 'show']);
+
+// Route for "Skills" page
+Route::get('/skills', [SkillsController::class, 'show']);
+
+// Route for "Hobbies" page
+Route::get('/hobbies', [HobbiesController::class, 'show']);
 
